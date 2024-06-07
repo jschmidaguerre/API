20be9ac235932700ce45412c599f6a41e750379b
gpedrosad <gpedrosa@uade.edu.ar>
2024-06-07 16:14:16 -0300

    agregando tailwind y config

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // asegúrate de que esta ruta coincida con tus archivos de componente
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
