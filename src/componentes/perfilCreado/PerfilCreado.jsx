e1377b85480444052afea3a35b37b2c009915a83
gpedrosad <gpedrosa@uade.edu.ar>
2024-06-24 15:52:38 -0300

    arreglado componente perfil creado

--- src/componentes/perfilCreado/PerfilCreado.jsx
+++ src/componentes/perfilCreado/PerfilCreado.jsx
@@ -1,27 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const PerfilCreado = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false); //Solo aparece cuando está en true
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Solo aparece cuando está en true
   const navigate = useNavigate();
 
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate('/login');
+    }
+  }, [isLoggedIn, navigate]); // Dependencias para asegurar que el efecto se ejecuta solo cuando cambia isLoggedIn
+
   if (!isLoggedIn) {
-    navigate('/login'); 
-    return null;
+    return null; // Retornar null mientras espera por la condición o redirección
   }
 
   return (
-        <div class="container mx-auto flex w-full justify-evenly">
-            <div class="text-side flex flex-col items-center justify-center">
-                <img src="images/verificado.png" alt="verificado" className='w-8'/>
-                <h1 className='text-3xl my-4'>¡Completado!</h1>
-                <p className='m-2'>Tu perfil se ha creado</p>
-            <button class="md:m-4 bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-8 rounded-xl ">Buscar Sitter</button>
-        </div>
-            <div class="image-side">
-                <img src="images/perfilCreado.png" alt="Imagen" />
-            </div>
-        </div>
+    <div className="container mx-auto flex w-full justify-evenly">
+      <div className="text-side flex flex-col items-center justify-center">
+        <img src="images/verificado.png" alt="verificado" className='w-8'/>
+        <h1 className='text-3xl my-4'>¡Completado!</h1>
+        <p className='m-2'>Tu perfil se ha creado</p>
+        <button className="md:m-4 bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-8 rounded-xl ">Buscar Sitter</button>
+      </div>
+      <div className="image-side">
+        <img src="images/perfilCreado.png" alt="Imagen" />
+      </div>
+    </div>
   );
 };
 
