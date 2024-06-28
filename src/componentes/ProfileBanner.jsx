410f7149c9338bdfc0c96799dd6a9c90048c13e1
gpedrosad <gpedrosa@uade.edu.ar>
2024-06-28 14:23:28 -0300

    seccion de reseñas

--- src/componentes/ProfileBanner.jsx
+++ src/componentes/ProfileBanner.jsx
@@ -20,7 +20,10 @@ const ProfileBanner = () => {
         <button className="text-3xl cursor-pointer" onClick={() => handleClick('Mis reservas')}>📅</button>
         <div className="text-sm mt-2">Mis reservas</div>
       </div>
-
+      <div className="text-center">
+        <button className="text-3xl cursor-pointer" onClick={() => handleClick('Mis reseñas')}>🌟</button>
+        <div className="text-sm mt-2">Mis reseñas</div>
+      </div>
     </div>
   );
 };
